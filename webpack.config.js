4d5b4c51db5c035f61fda63206c79ee41aa7831b
董星雨 <1285480439@qq.com>
2021-04-06 14:16:47 +0800

    HtmlWebpackPlugin/webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin'); // 引入压缩插件
 var HotModuleReplacementPlugin = require('webpack').HotModuleReplacementPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'none',
@@ -13,7 +14,7 @@ module.exports = {
     // vendors: ['react-resizable'],
   },
   output: {
-    path: path.join(__dirname, 'dist/lib'),
+    path: path.join(__dirname, 'dist'),
     library: 'resizableTable',
     libraryTarget: 'umd',
     libraryExport: 'default', // 不添加的话引用的时候需要 xxx.default
@@ -23,7 +24,7 @@ module.exports = {
     extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx'],
   },
   resolveLoader: {
-    // 因为 html-loader 是开源 npm 包，所以这里要添加 'node_modules' 目录
+    // loader搜索目录，包括自己写的，还要添加 'node_modules' 目录
     modules: [path.join(__dirname, './src/loaders'), 'node_modules'],
   },
   optimization: {
@@ -37,11 +38,12 @@ module.exports = {
   },
   devServer: {
     // 开发服务器
-    contentBase: path.join(__dirname, 'dist/lib'), // 发布目录
+    contentBase: path.join(__dirname, 'dist'), // 发布目录
     host: 'localhost',
     // compress: true,
     port: 6030,
     hot: true,
+    inline: true, // 实时更新
     open: true, // 是否默认打开浏览器
   },
   module: {
@@ -90,15 +92,15 @@ module.exports = {
           //       ) {
           //         return localName;
           //       }
-          //       const match = context.resourcePath.match(/src(.*)/);
-          //       if (match && match[1]) {
-          //         const antdProPath = match[1].replace('.less', '');
-          //         const arr = slash(antdProPath)
-          //           .split('/')
-          //           .map(a => a.replace(/([A-Z])/g, '-$1'))
-          //           .map(a => a.toLowerCase());
-          //         return `dodo-${arr.join('-')}-${localName}`.replace(/--/g, '-');
-          //       }
+          //       // const match = context.resourcePath.match(/src(.*)/);
+          //       // if (match && match[1]) {
+          //       //   const antdProPath = match[1].replace('.less', '');
+          //       //   const arr = slash(antdProPath)
+          //       //     .split('/')
+          //       //     .map(a => a.replace(/([A-Z])/g, '-$1'))
+          //       //     .map(a => a.toLowerCase());
+          //       //   return `dodo-${arr.join('-')}-${localName}`.replace(/--/g, '-');
+          //       // }
           //       return localName;
           //     },
           //   },
@@ -165,7 +167,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HotModuleReplacementPlugin()],
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    // 以该文件下的本地index.html作为模板,打包的时候自动生成服务器html并自动引入打包的js文件
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'dist/index.html'),
+    }),
+  ],
   devtool: 'source-map',
   // 使自己项目中依赖于宿主项目里的库，不重复打包,比如react，因为引入的肯定是react项目，所以不需要再将react打包进npm包
   externals: {
