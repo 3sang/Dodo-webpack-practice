207abb30b3080e60195cc52a7937d19cff917384
3sang <1285480439@qq.com>
2021-05-25 10:31:18 +0800

    尝试提交测试

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Index from './pages';
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App';
 
 ReactDom.render(<Index />, document.getElementById('app'));
 
